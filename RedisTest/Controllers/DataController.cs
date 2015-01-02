c12e901a707349eeffaf0fb517889f60e4feccde
Martin Wåger <martin.w@comaround.se>
2015-01-02 01:10:24 +0100

    with (failing) json de+serialization

--- RedisTest/Controllers/DataController.cs
+++ RedisTest/Controllers/DataController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Web.Http;
+using Newtonsoft.Json;
 using StackExchange.Redis;
 
 namespace RedisTest.Controllers
@@ -27,25 +29,75 @@ namespace RedisTest.Controllers
             {
                 return Get();
             }
-            string result;
+            ContentDto result;
             var redisdb = Redis.GetDatabase();
-            var data = redisdb.StringGet(string.Format("data with key {0}", id));
-            if (data.IsNullOrEmpty)
+            var resultString = redisdb.StringGet(string.Format("data with key {0}", id));
+            if (resultString.IsNullOrEmpty)
             {
-                result = GetValueFromDataSource();
-                redisdb.StringSet(string.Format("data with key {0}", id), result, TimeSpan.FromSeconds(20));
+                result = GetValueFromDataSource(id);
+                resultString = JsonConvert.SerializeObject(result);
+                redisdb.StringSet(string.Format("data with key {0}", id), resultString, TimeSpan.FromSeconds(20));
             }
             else
             {
-                result = data;
+                result = JsonConvert.DeserializeObject<ContentDto>(resultString);
             }
             return Ok(result);
         }
 
-        private static string GetValueFromDataSource()
+        private static ContentDto GetValueFromDataSource(int id)
         {
             Thread.Sleep(1 * 1000);
-            return "Everything is Awesome!";
+            return new ContentDto
+            {
+                ContentId = id,
+                CreatedDate = DateTime.UtcNow,
+                OwnerPortalId = 23,
+                BasedOnId = 34,
+                Translations = new List<TranslationDto>
+                {
+                    new TranslationDto
+                    {
+                        CreatedDate = DateTime.UtcNow,
+                        Id = id,
+                        Name = "Name",
+                        PublishState = PublishState.Aa,
+                        UpdatedDate = null
+                    }
+                },
+                Type = ContentType.Folder
+            };
         }
+    }
+
+    public class ContentDto
+    {
+        public int ContentId { get; internal set; }
+        public DateTime CreatedDate { get; internal set; }
+        public int OwnerPortalId { get; internal set; }
+        public int BasedOnId { get; internal set; }
+        public List<TranslationDto> Translations { get; internal set; }
+        public ContentType Type { get; internal set; }
+    }
+    public class TranslationDto
+    {
+        public int Id { get; internal set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; internal set; }
+        public DateTime? UpdatedDate { get; set; }
+        public PublishState PublishState { get; set; }
+    }
+    public enum ContentType
+    {
+        Folder,
+        Guide,
+        Link
+    }
+
+    public enum PublishState
+    {
+        Aa,
+        Bb,
+        Cc
     }
 }
